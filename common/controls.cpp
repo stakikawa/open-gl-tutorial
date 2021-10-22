9f1f40a16d5144048065ac5f2f189914eb19a166
stakikawa <suzuran.takikawa@gmail.com>
2021-10-22 01:15:50 -0700

    Edited controls to allow for vertical movement

--- common/controls.cpp
+++ common/controls.cpp
@@ -87,10 +87,18 @@ void computeMatricesFromInputs(){
 	if (glfwGetKey( window, GLFW_KEY_LEFT ) == GLFW_PRESS){
 		position -= right * deltaTime * speed;
 	}
+	// Move up
+	if (glfwGetKey( window, GLFW_KEY_Q ) == GLFW_PRESS){
+		position += up * deltaTime * speed;
+	}
+	// Move down
+	if (glfwGetKey( window, GLFW_KEY_E ) == GLFW_PRESS){
+		position -= up * deltaTime * speed;
+	}
 
 	float FoV = initialFoV;// - 5 * glfwGetMouseWheel(); // Now GLFW 3 requires setting up a callback for this. It's a bit too complicated for this beginner's tutorial, so it's disabled instead.
 
-	// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+	// Projection matrix : 45ï¿½ Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
 	ProjectionMatrix = glm::perspective(glm::radians(FoV), 4.0f / 3.0f, 0.1f, 100.0f);
 	// Camera matrix
 	ViewMatrix       = glm::lookAt(
